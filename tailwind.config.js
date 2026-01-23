bf1739915da9061f964c4825d3305a924fecb77e
仨儿 <819567884@qq.com>
2026-01-23 13:21:15 +0800

    样式本地化

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './prototypes/**/*.html',
+    './prototypes/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
